aac2de3221110d48387efa36dbc9c898e715a894
Joshua Rieth <joshua.m.rieth@gmail.com>
2020-10-02 20:28:31 -0400

    complete searching.py

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,12 @@
-def linear_search(arr, target):
-    # Your code here
 
+def linear_search(arr, target):
+
+    index = 0
+    while index != len(arr):
+        if arr[index] == target:
+            return index
+        else:
+             index += 1
 
     return -1   # not found
 
@@ -8,7 +14,22 @@ def linear_search(arr, target):
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-    # Your code here
 
+    # initialize start and end
+    start = 0
+    end = len(arr) - 1
+
+    while end >= start:
+        # Find the middle
+        middle = (start + end) // 2
+        # Compare middle to target
+        if target == arr[middle]:
+            return middle
+
+        if target > arr[middle]:
+            start = middle + 1
+
+        if target < arr[middle]:
+            end = middle - 1
 
     return -1  # not found
